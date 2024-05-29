ac92848fb06147db072e6086b3b31f641fff37d5
Dmytro Belymenko <41953956+horizoncoder@users.noreply.github.com>
2024-05-29 14:23:46 +0300

    Update route.js

--- src/app/api/send-email/route.js
+++ src/app/api/send-email/route.js
@@ -1,48 +1,7 @@
-import mailgun from "mailgun-js";
 
-const KEY = process.env.API_KEY;
-const DOMAIN = process.env.DOMAIN;
-const WEATHER_KEY = process.env.WEATHER_API_KEY;
 
 export async function GET(req, res) {
-  const mg = mailgun({apiKey: KEY, domain: DOMAIN});
-
-  const city = 'New York, NY';
-  const countryCode = 'US';
-
-  const locationEndpoint = `http://dataservice.accuweather.com/locations/v1/cities/${countryCode}/search`;
-  const locationRequest = await fetch(`${locationEndpoint}?q=${encodeURIComponent(city)}&apikey=${WEATHER_KEY}`);
-  const locationData = await locationRequest.json();
-
-  const locationKey = locationData[0].Key;
-  const forecastEndpoint = `http://dataservice.accuweather.com/forecasts/v1/daily/1day/${locationKey}`;
-  const forecastRequest = await fetch(`${forecastEndpoint}?apikey=${WEATHER_KEY}`);
-  const forecastData = await forecastRequest.json();
-
-  const data = {
-    from: 'Weather News <mailgun@VERCEL_CRON_EMAIL>',
-    to: 'forthosewhocode@gmail.com',
-    subject: 'Your Daily Weather Report',
-    html: `
-    <h1>${forecastData?.Headline?.Text}</h1>
-    <p>${forecastData?.Headline?.Category}</p>
-    <ul>
-    <li>Temp Min: ${forecastData.DailyForecasts[0].Temperature.Minimum.Value}° ${forecastData.DailyForecasts[0].Temperature.Minimum.Unit}</li>
-    <li>Temp Max: ${forecastData.DailyForecasts[0].Temperature.Maximum.Value}° ${forecastData.DailyForecasts[0].Temperature.Maximum.Unit}</li>
-    </ul>
-    <button>${forecastData?.Headline?.Link}</button>
-    `
-  };
-
-  mg.messages().send(data, function (error, body) {
-    if(error){
-      console.log(error);
-      res.status(500).send({ message: 'Error in sending email' });
-    }
-    console.log(body?.message);
-  });
-
-  return new Response('Email was sent!', {
-    status: 200
-  });
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json))
 }
